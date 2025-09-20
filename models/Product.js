c3dfdc839dee444141dea537b05ee6f6871471ad
HuaMinhKhuong-22700971 <khuongminhhua11062004@gmail.com>
2025-09-20 18:06:49 +0700

    Phan 1 sản phẩm với Bootstrap

--- models/Product.js
+++ models/Product.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
+
 const ProductSchema = new mongoose.Schema({
-  name: String,
-  address: String,
-  phone: String,
-  supplierId: { type: mongoose.Schema.Types.ObjectId, ref: 'Supplier' }
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  quantity: { type: Number, required: true },
+  supplierId: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Supplier',
+    required: true
+  }
 });
+
 module.exports = mongoose.model('Product', ProductSchema);
